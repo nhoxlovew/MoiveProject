e2dcafb7178de04655acb66e2a0b995051d26763
Hoàng Anh <79311236+linustechtip@users.noreply.github.com>
2024-12-29 03:43:26 +0700

    fix scollbar v1.1

--- js/anhflix.js
+++ js/anhflix.js
@@ -1,7 +1,9 @@
 const movieContainer = document.querySelector('.movie-card-container');
 const apiURL = "https://ophim1.com/danh-sach/phim-moi-cap-nhat?page=1";
+let currentPage = 1;
+let totalPages = 1;
 
-async function loadMovies() {
+async function loadMovies(page = 1) {
     try {
         const response = await fetch(apiURL);
         const data = await response.json();
@@ -12,14 +14,11 @@ async function loadMovies() {
         const movies = data.items;
         console.log(movies.name);
         const baseImageUrl = data.pathImage;
-
-
-        //Cho mỗi hàng 5 phim
-        const row = [];
-
+        totalPages = data.pagination.totalPages;
+        currentPage = page
         // Chèn dữ liệu vào container
-        movieContainer.innerHTML = 
-        movies.map((item)=>`
+        movieContainer.innerHTML =
+            movies.map((item) => `
             <div class="card">
                     <img class="img-card" src="${baseImageUrl}${item.thumb_url}" alt="Movie Title">
                     <div class="content">
@@ -27,14 +26,17 @@ async function loadMovies() {
                     </div>
                 </div>
             `).join('');
-        
 
-        // Generate movie cards
-        
+        document.getElementById('prevBtn').disabled = page <= 1;
+        document.getElementById('nextBtn').disabled = page >= totalPages;
+        document.getElementById('pageInfo').textContent = `Page ${page} of ${totalPages}`;
     } catch (error) {
         console.error("Error loading movies:", error);
     }
 }
 
+
 loadMovies()
+
+
 

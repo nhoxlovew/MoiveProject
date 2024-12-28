187509be4c3542fa5fc3b698629401e5c8f036ed
Hoàng Anh <79311236+linustechtip@users.noreply.github.com>
2024-12-29 02:32:07 +0700

    fetch data api vào card container

--- js/anhflix.js
+++ js/anhflix.js
@@ -1,29 +1,40 @@
+const movieContainer = document.querySelector('.movie-card-container');
 const apiURL = "https://ophim1.com/danh-sach/phim-moi-cap-nhat?page=1";
 
 async function loadMovies() {
     try {
         const response = await fetch(apiURL);
         const data = await response.json();
-        const movies = data.items; // Adjust based on actual API response structure
-        const movieContainer = document.querySelector('.movie-card');
 
-        // Clear existing content
-        movieContainer.innerHTML = '';
+        // Debug: Check the API response
+        console.log(data);
+
+        const movies = data.items;
+        console.log(movies.name);
+        const baseImageUrl = data.pathImage;
+
+
+        //Cho mỗi hàng 5 phim
+        const row = [];
+
+        // Chèn dữ liệu vào container
+        movieContainer.innerHTML = 
+        movies.map((item)=>`
+            <div class="card">
+                    <img class="img-card" src="${baseImageUrl}${item.thumb_url}" alt="Movie Title">
+                    <div class="content">
+                        <span>${item.name}</span>
+                    </div>
+                </div>
+            `).join('');
+        
 
         // Generate movie cards
-        movies.forEach(movie => {
-            const card = document.createElement('div');
-            card.classList.add('card');
-            card.innerHTML = `
-                    <img class="img-card" src="${movie.image}" alt="${movie.title}">
-                    <div class="content">${movie.title}</div>
-                `;
-            movieContainer.appendChild(card);
-        });
+        
     } catch (error) {
         console.error("Error loading movies:", error);
     }
 }
 
-document.addEventListener('DOMContentLoaded', loadMovies);
-///dasd
+loadMovies()
+
